6a8740a9e391f5733fb4038b4bcbdf7cf36b5af5
LisaLyakh <99537140+LisaLyakh@users.noreply.github.com>
2022-12-13 13:37:25 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import { ToastContainer } from 'react-toastify';
+import Container from './App.styled';
+
+class App extends Component {
+  state = {
+    searchQuery: '',
+  };
+
+  handleFormSubmit = searchQuery => {
+    this.setState({ searchQuery });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ToastContainer autoClose={3000} />
+        <ImageGallery searchQuery={this.state.searchQuery} />
+      </Container>
+    );
+  }
+}
+
+export default App;
