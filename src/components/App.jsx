e9c404ddb2b63946fc578b2858fff045b2f0da12
LisaLyakh <99537140+LisaLyakh@users.noreply.github.com>
2022-12-18 22:53:24 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,11 @@ import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import { ToastContainer } from 'react-toastify';
 import Container from './App.styled';
+import Button from './Button/Button'
+// import LoadMore from './Button/Button';
+// import Modal from './Modal/Modal';
+// import Loader from './Loader/Loader';
+
 
 class App extends Component {
   state = {
@@ -14,14 +19,17 @@ class App extends Component {
   };
 
   render() {
+    const { images, loading } = this.state;
     return (
       <Container>
-        <Searchbar onSubmit={this.handleFormSubmit} />
+        <Searchbar onSubmit={this.onSubmit} />
         <ToastContainer autoClose={3000} />
         <ImageGallery searchQuery={this.state.searchQuery} />
+        {images.length !== 0 && !loading && (
+          <Button LoadMore={this.LoadMore}
+         />)}
       </Container>
     );
   }
 }
-
 export default App;
